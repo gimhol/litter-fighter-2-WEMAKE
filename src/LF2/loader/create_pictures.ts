a251d8b5981b4df7e860e60a2f969cb915bcb0ee
gim_z66 <gimholg1992@foxmail.com>
2025-11-22 10:32:31 +0800

    refactor: Pay the price for over-encapsulation

--- src/LF2/loader/create_pictures.ts
+++ src/LF2/loader/create_pictures.ts
@@ -3,7 +3,7 @@ import IPicture from "../defines/IPicture";
 import { TData } from "../entity/Entity";
 import type { LF2 } from "../LF2";
 
-export default function create_pictures(lf2: LF2, data: TData) {
+export default function create_pictures(lf2: LF2, data: TData): Map<string, IPicture<THREE.Texture>> {
   const pictures = new Map<string, IPicture<THREE.Texture>>();
   const {
     base: { files },
